028a799df4935eaf70ab531633adea7baf3c089a
valeriuflorescu16 <valeriuflorescu16@users.noreply.github.com>
2020-12-12 19:51:42 +0000

    script.js and assignment.js modified

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,32 @@
+const country = "Romania";
+const continent = "Europe";
+let population = 20;
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof language);
+console.log(typeof country);
+console.log(typeof continent);
+console.log(typeof population);
+
+language = "Romanian";
+
+let halfPopulation = population / 2;
+population++;
+let finlandPopulation = 6;
+let morePeople = population > finlandPopulation;
+let avgPopulation = 33;
+let lessThanAverage = population < avgPopulation;
+let description = "Portugal is in Europe, and its 11 million people speak Portuguese";
+
+console.log(halfPopulation);
+console.log(population);
+console.log(finlandPopulation);
+console.log(morePeople);
+console.log(avgPopulation);
+console.log(lessThanAverage);
+console.log(description);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,70 @@
+/* let js = "amazing";
+console.log(32 + 312 + 42);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Valeriu";  //declaring a variable
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let x = 27;
+console.log(x);
+
+let myFirstJob = "Concierge";
+let myCurrentJob = "Student";
+
+console.log(myFirstJob);
+console.log(myCurrentJob); */
+
+let valeriuIsSmart = true;
+console.log(valeriuIsSmart);
+console.log(typeof valeriuIsSmart);
+console.log(typeof 30);
+console.log(typeof "Salutare");
+
+valeriuIsSmart = "Yes, that is very true bro";
+console.log(valeriuIsSmart);
+console.log(typeof valeriuIsSmart);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1999;
+console.log(typeof year);
+console.log(year);
+console.log(typeof null);
+
+
+let age = 20;
+age = 21;
+
+const birthYear = 1999;
+var job = "Programmer";
+job = "Software Engineer";
+console.log(job);
+
+const now = 2020;
+const ageJack = now - 1998;
+const ageEmma = now - 1999;
+
+console.log(ageJack, ageEmma);
+console.log(ageJack * 2, ageJack / 10, 2 ** 3);  // ** power of .. 
+
+const firstName = "Valeriu";
+const lastName = "Florescu";
+console.log(firstName + ' ' + lastName);
+//operators
+let x = 69 + 420;
+x += 10;
+x *= 2;
+x++;
+console.log(x);
+
+console.log(ageJack > ageEmma);
+console.log(ageEmma >= 22);
+
+const isSheAnAdult = ageEmma >= 18;
